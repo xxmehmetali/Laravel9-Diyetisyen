665dd7bd85b1b2d6c293bc0c9c64c92f61863b09
xxmehmetali <xxmehmetali@hotmail.com>
2022-04-26 18:14:22 +0300

    Diyetisyen arama sayfası oluşturuldu (Kabataslak ve statik). 26.04.2022

---
+++ resources/views/layouts/home/browseDietitian-L.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="{{ asset('assets') }}/css/dietitians.css">
+@include("home.pageParts.header")
+
+<body>
+
+@include("home.pageParts.topbar")
+@include("home.pageParts.navbar")
+
+@include("home.pageParts.browseDietitian")
+
+@include("home.pageParts.footer")
+</body>
+</html>
